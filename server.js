ee61ae4e1ca2c7180f6d6a59df577c08f2823b7f
BillyZhong <billyczhong@gmail.com>
2018-01-01 05:22:48 -0800

    Server file

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res){
-  res.send("Dynosaur coming soon!");
-});
-
 app.get('*', function(req,res){
   res.sendFile('/home/billy/dynosaur/' + req.path);
 });
